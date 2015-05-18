44f1f75e3e4b9de15b27302571c44880effaae35
seanvdburg <seanvanderburg@msn.com>
2015-05-18 15:13:22 +0200

    Initial commit
    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,138 @@
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+@SuppressWarnings("serial")
+public class Main extends JFrame implements ActionListener {
+	
+	JPanel mypanel;
+	JTextField input;
+	JLabel result;
+	JLabel introtext;
+	JButton next;
+	JButton back;
+	JLabel imageLabel;
+	ImageIcon image;
+	int direction = 0;
+	int correctIterator = 0;
+	String inputValue;
+	
+	
+	ArrayList<String> images = new ArrayList<String>();
+	ArrayList<Integer> correct = new ArrayList<Integer>();
+
+	public static void main(String[] args) {
+		Main frame = new Main();
+		frame.setTitle("Kleurenblindheidstest");
+		frame.setSize(280, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.setResizable(false);
+	}
+
+	public Main() {
+		images.add("Fotos/Test5.jpg");
+		images.add("Fotos/Test8.jpg");
+		images.add("Fotos/Test56.jpg");
+		images.add("Fotos/Test25.jpg");
+		images.add("Fotos/Test29.jpg");
+		images.add("Fotos/Test6.jpg");
+		images.add("Fotos/Test3.jpg");
+		
+		correct.add(5);
+		correct.add(8);
+		correct.add(56);
+		correct.add(25);
+		correct.add(29);
+		correct.add(6);
+		correct.add(3);
+
+		image = new ImageIcon(images.get(0).toString());
+		input = new JTextField("", 15);
+		result = new JLabel();
+		introtext = new JLabel("Voer het getal in dat op de afbeelding is te zien.");
+		next = new JButton("Volgende");
+		back = new JButton("Vorige");
+		back.addActionListener(this);
+		next.addActionListener(this);
+		mypanel = new JPanel();
+		imageLabel = new JLabel();
+
+		Font bigFont = mypanel.getFont().deriveFont(Font.PLAIN, 15f);
+		input.setFont(bigFont);
+
+		FlowLayout experimentLayout = new FlowLayout();
+		this.add(mypanel);
+		
+		imageLabel.setIcon(image);
+		mypanel.setLayout(experimentLayout);
+		mypanel.add(introtext);
+		mypanel.add(imageLabel);
+		mypanel.add(input);
+		mypanel.add(next);
+		mypanel.add(back);
+		mypanel.add(result);
+	}
+
+	public void actionPerformed(ActionEvent event) {
+		if (event.getSource() == next) {
+			inputValue = input.getText();
+			System.out.println("Verwacht antwoord: " + correct.get(direction));
+			System.out.println("Ingevoerd antwoord: " + inputValue);
+			System.out.println("----------------------------------------------------");
+			if (correct.get(direction).toString().equals(inputValue)) {
+				correctIterator++;
+				direction++;
+				if (direction == images.size()) {
+					direction = 0;
+					System.out.println("Je hebt " + correctIterator	+ " van de 7 getallen goed ingevuld");
+					result.setText("Je hebt " + correctIterator	+ " van de 7 getallen goed ingevuld");
+					correctIterator = 0;
+				}
+				if (direction == 1){
+					result.setText("");
+				}
+				image = new ImageIcon(images.get(direction).toString());
+				imageLabel.setIcon(image);
+			}
+
+			else {
+				direction++;
+				if (direction == images.size()) {
+					direction = 0;
+					System.out.println("Je hebt " + correctIterator	+ " van de 7 getallen goed ingevuld");
+					result.setText("Je hebt " + correctIterator	+ " van de 7 getallen goed ingevuld");
+					correctIterator = 0;
+				}
+				if (direction == 1){
+					result.setText("");
+				}
+				image = new ImageIcon(images.get(direction).toString());
+				imageLabel.setIcon(image);
+			}
+		}
+
+		else if (event.getSource() == back) {
+			if (direction != 0) {
+				direction--;
+				correctIterator--;
+				if(direction == 0){
+					correctIterator = 0;
+				}
+				image = new ImageIcon(images.get(direction).toString());
+				imageLabel.setIcon(image);
+			} else {
+				image = new ImageIcon(images.get(direction).toString());
+				imageLabel.setIcon(image);
+			}
+		}
+	}
+}
